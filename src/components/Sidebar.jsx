7a97389d3fc280929ad90cffdad749c00ffcb418
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2025-10-05 20:45:25 +0530

    implement nested route

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,16 @@
+import { Outlet } from "react-router";
 import AppNav from "./AppNav";
 import Footer from "./Footer";
 import Logo from "./Logo";
 import styles from "./Sidebar.module.css";
+
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
 
-      <p>List of cities</p>
+      <Outlet />
 
       <Footer />
     </div>
