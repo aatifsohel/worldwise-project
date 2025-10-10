181601d651267e56c6398f63240e270f08598a81
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2025-10-10 22:39:13 +0530

    add intractivity with the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,16 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvent,
+} from "react-leaflet";
 import styles from "./Map.module.css";
-import { useNavigate } from "react-router";
+import { useNavigate, useSearchParams } from "react-router";
 import { useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
+import { useEffect } from "react";
 
 const flagemojiToPNG = flag => {
   var countryCode = Array.from(flag, codeUnit => codeUnit.codePointAt())
@@ -14,11 +22,20 @@ const flagemojiToPNG = flag => {
 };
 
 function Map() {
-  const navigate = useNavigate();
-
   const { cities } = useCities();
-
+  const [searchParams] = useSearchParams();
   const [mapPosition, setMapPosition] = useState([40, 0]);
+
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
+
   return (
     <div className={styles.mapWrapper}>
       <MapContainer
@@ -43,9 +60,29 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+
+        {/* adding created component */}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
+}
+
+function ChangeCenter({ position }) {
+  // a hook from leaflet
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+
+  // a hook & event from leaflet
+  useMapEvent({
+    click: e => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
 }
 
 export default Map;
